b2b71132a2d2639f2269325b99dc2bad3f4e149c
jadenaguilon <jadaguilon@gmail.com>
2026-02-04 19:02:31 -0500

    Initialize Express server and Spotify auth setup

--- server.js
+++ server.js
@@ -5,21 +5,26 @@ import cookieParser from "cookie-parser"; // lets you read cookies
 import crypto from "crypto"; // Secure random and SHA256
 import path from "path";
 import { fileURLToPath } from "url";
+import res from "express/lib/response";
 
-dotenv.config(); // Read in secret data from .env
+const STATE_BYTE_LENGTH = 16;
+const VERIFY_BYTE_LENGTH = 32;
+// Read in secret data from .env
+dotenv.config();
 
-const app = express(); // App creation
+// App creation
+const app = express();
 
 app.use(cookieParser()); // Allows you to request cookie info
-app.use(express.static("public")); // Simplifies: GET /index.html -> public/index.html
+app.use(express.static("public")); // Simplifies: GET /index.html -> /public/index.html
 
-const __filename = fileURLToPath(import.meta.url); // /Users/jaden/project/server.js
-const __dirname = path.dirname(__filename); // /Users/jaden/project
+// Saving file name and directory of server.js
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, REDIRECT_URI } = process.env; // Load from process.env
 
-// Used to add SecureRandom secret to URL to prove you're the original user
-// https://accounts.spotify.com/authorize? + code_challenge=Ab-c_def...
+// URL Format: https://accounts.spotify.com/authorize? + code_challenge=Ab-c_def...
 // Mute complete code_challenge to access
 function base64url (buffer){
     return buffer
@@ -29,6 +34,17 @@ function base64url (buffer){
         .replace(/=+$/, "");
 }
 
-// --- LOGIN AUTHENTIFICATION + HANDSHAKE---
+// --- LOGIN AUTHENTIFICATION + CRYPTOGRAPHY HANDSHAKE---
+app.get("/login", async (req, res) => {
+    const state = crypto.randomBytes(STATE_BYTE_LENGTH).toString("hex");
+    const codeVerifier = base64url(crypto.randomBytes(VERIFY_BYTE_LENGTH));
+    const codeChallenge = base64url(
+        crypto.createHash("sha256").update(codeVerifier).digest()
+    );
 
+    // Stores to browser cookie and only the server (via HTTP requests)
+    // can access info, not JavaScript
+    res.cookie("spotify_auth_state", state, {httpOnly: true});
+    res.cookie("spotify_code_verifier", codeVerifier, {httpOnly: true});
 
+});
