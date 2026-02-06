833fcdd0d074f072aea58e7a177376765a77ce78
jadenaguilon <jadaguilon@gmail.com>
2026-02-05 22:18:36 -0500

    Use dynamic PORT for deployment

--- server.js
+++ server.js
@@ -183,4 +183,5 @@ app.get("/logout", (req, res) => {
     res.redirect("/");
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
